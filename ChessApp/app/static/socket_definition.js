453ed55d8583b9aa1637acb251b3e5711b2b9761
Jordan de Burgo <jordandeburgo@gmail.com>
2022-06-16 00:15:11 +0100

    try

--- ChessApp/app/static/socket_definition.js
+++ ChessApp/app/static/socket_definition.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     socket = io.connect();
     console.log(socket);
     socket.on('connect', function() {
+        console.log("CONNECTING...")
         socket.emit('join', {});
     });
 });
