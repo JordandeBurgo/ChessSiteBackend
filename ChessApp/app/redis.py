6a4667cfbe66e4eabe9a753870c913a66fb18813
Jordan de Burgo <jordandeburgo@gmail.com>
2022-06-15 18:42:17 +0100

    ping

--- ChessApp/app/redis.py
+++ ChessApp/app/redis.py
@@ -5,4 +5,7 @@ import redis
 def get_r():
     url = urlparse(os.environ.get("REDIS_URL"))
     r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+    
+    r.ping() 
+    
     return r
