d65db5cc0fc4bb1517da6193faf7ae6420299fd5
Jordan de Burgo <jordandeburgo@gmail.com>
2022-06-15 19:09:33 +0100

    more tests

--- ChessApp/app/redis.py
+++ ChessApp/app/redis.py
@@ -5,9 +5,10 @@ import redis
 def get_r():
     print("HELLOX")
     url = urlparse(os.environ.get("REDIS_URL"))
-    print("HELLOY ", url)
+    print("HELLOY ", url.hostname, " ", url.port, " ", url.username, " ", url.password)
     r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
     print("HELLOZ")
     print(r.ping())
+    print("HELLOA")
     
     return r
