e85568ab2614a69447165e72a7090733f09640db
Jordan de Burgo <jordandeburgo@gmail.com>
2022-06-17 00:19:42 +0100

    test

--- ChessApp/app/static/socket_definition.js
+++ ChessApp/app/static/socket_definition.js
@@ -1,7 +1,8 @@
 var socket;
 $(document).ready(function(){
     socket = io('/game');
-    let t = window.location.href.substring(window.location.href.lastIndexOf("/") + 1)
+    let t = window.location.href.substring(window.location.href.lastIndexOf("/") + 1);
+    console.log(socket);
     socket.on('connect', function() {
         socket.emit('join', {'id': t});
     });
